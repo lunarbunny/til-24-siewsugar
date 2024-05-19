a2a1f3193e7fadf8ef606cb4ff84cbbfb2de5271
lunarbunny <tingwei1734@gmail.com>
2024-05-19 10:54:28 +0000

    [VLM] YOLO based VLM pipeline

--- vlm/src/VLMManagerYolo.py
+++ vlm/src/VLMManagerYolo.py
@@ -10,7 +10,7 @@ class VLMManager:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print(f"[VLM] Device: {self.device}")
-        self.yolo_model = YOLO("yolov8x.pt")
+        self.yolo_model = YOLO("models/yolov8m-finetuned-100/weights/best.pt")
         self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(self.device)
         self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
@@ -18,24 +18,26 @@ class VLMManager:
         img = np.array(Image.open(io.BytesIO(image)))
         
         # Detect all objects in image
-        yolo_result = self.yolo_model(img, conf=0.1)[0]
-        yolo_objects = []
-        for bbox in yolo_result.boxes.xywh:
-            # Box is a Tensor of [x, y, w, h]
-            # x, y is the center
-            x, y, w, h = bbox.tolist()
-            # Crop bounding boxes from orginal image
-            top, left = round(y-(h/2)), round(x-(w/2))
-            cropped = img[top:int(top+h), left:int(left+w), ::-1]
-            yolo_objects.append((cropped, [left, top, int(w), int(h)]))
+        yolo_result = self.yolo_model(img, half=True, conf=0.1)[0]
+        yolo_annotations = []
 
-        if len(yolo_objects) == 0:
+        for box, cls in zip(yolo_result.boxes.xywh, yolo_result.boxes.cls):
+            # Box is a Tensor of [x, y, w, h]
+            x, y, w, h = box.tolist()
+            # Crop bounding boxes from orginal image
+            left, top, w, h = int(x-(w/2)), int(y-(h/2)), int(w), int(h)
+            cropped = yolo_result.orig_img[top:top+h, left:left+w, ::-1] # BGR to RGB
+            obj_class = yolo_model.names[cls.item()]
+            #print(f"[{left}, {top}, {int(w)}, {int(h)}] {cls.int().item()} {obj_class}")
+            yolo_annotations.append({"image": cropped, "bbox": [left, top, w, h], "class": f"{obj_class}"})
+
+        if len(yolo_annotations) == 0:
             return [0, 0, 0, 0]
         
-        inputs = self.clip_processor(text=caption, images=[o[0] for o in yolo_objects], return_tensors="pt", padding=True).to(self.device)
+        inputs = self.clip_processor(text=caption, images=[a["image"] for a in yolo_annotations], return_tensors="pt", padding=True).to(self.device)
         with torch.no_grad():
             outputs = self.clip_model(**inputs)
         logits_per_image = outputs.logits_per_image # image-text similarity score
         best_match_idx = logits_per_image.argmax(dim=0).item()
 
-        return yolo_objects[best_match_idx][1]
+        return yolo_annotations[best_match_idx]["bbox"]
